91696419e0273ebb742fc755212cb5e3f907354b
Colin Koo <chkoo@cpp.edu>
2017-04-04 23:18:33 -0700

    multi is a mess

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,37 +9,49 @@ import java.net.Socket;
 
 public final class EchoServer {
 
-    public static void main(String[] args) throws Exception {
-    	//ServerSocket(int port)
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-            	
-                try (Socket socket = serverSocket.accept()) {
-                	InputStream is = socket.getInputStream();
-                	InputStreamReader isr = new InputStreamReader(is);
-                	BufferedReader in = new BufferedReader(isr); 
-                	
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                   
-                    
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    
-                    //out.println("Hi " + address + ", thanks for connecting!");
-                    while (true){
-                    	String input = in.readLine();
-                    	if (!input.toLowerCase().equals("exit")){
-                    		out.println("Server> " + input);
-                    		out.flush();
-                    	}
-                    	else{
-                            System.out.printf("Client disconnected: %s%n", address);
-                            break;
-                    	}
-                    }
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+
+		try (ServerSocket serverSocket = new ServerSocket(22222)) {
+			while (true) {
+
+				try (Socket socket = serverSocket.accept()) {
+
+					String address = socket.getInetAddress().getHostAddress();
+					System.out.printf("Client connected: %s%n", address);
+					
+					
+					new multiClient(socket).start();
+					//out.println("Hi " + address + ", thanks for connecting!");
+
+				}
+			}
+		}
+	}
+	private static class multiClient extends Thread {
+		Socket socket;
+		public multiClient(Socket socket){
+			this.socket = socket;
+		}
+
+		public void run(){
+			try	{
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader in = new BufferedReader(isr); 
+				OutputStream os = socket.getOutputStream();
+				PrintStream out = new PrintStream(os, true, "UTF-8");
+				while (true){
+					String input = in.readLine();
+					if (!input.toLowerCase().equals("exit")){
+						out.println("Server> " + input);
+						out.flush();
+					}
+					else{
+						System.out.printf("Client disconnected: %s%n", address);
+						break;
+					}
+				}
+			} catch (Exception e){ }
+		}
+	}
 }
